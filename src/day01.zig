f001206997e35672998a3db83ef2a52a0cf8dbce
Eli Gukovsky <gukovskye@gmail.com>
2024-07-03 00:32:39 -0400

    Day 1a, backbone is more set up for future parts

--- src/day01.zig
+++ src/day01.zig
@@ -5,10 +5,43 @@ pub fn main() !void {
     try stdout.print("Hello, {s}!\n", .{"world"});
 }
 
-test "simple day 1" {
-    std.debug.print("Simple test for day 1.\n", .{});
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn day01a(allocator: std.mem.Allocator, file: []const u8) ![]const u8 {
+    var line_iterator = std.mem.splitSequence(u8, file, "\n");
+    var curr: u64 = undefined;
+    if (line_iterator.next()) |line| {
+        curr = try std.fmt.parseInt(u64, line, 10);
+    }
+    var up_counter: u64 = 0;
+    while (line_iterator.next()) |line| {
+        if (line.len == 0) {
+            break;
+        }
+        const next: u64 = try std.fmt.parseInt(u64, line, 10);
+        if (next > curr) {
+            up_counter += 1;
+        }
+        curr = next;
+    }
+    return std.fmt.allocPrint(allocator, "{d}", .{up_counter});
+}
+
+test "Day 1a" {
+    std.debug.print("Day 1a\n", .{});
+    const file =
+        \\199
+        \\200
+        \\208
+        \\210
+        \\200
+        \\207
+        \\240
+        \\269
+        \\260
+        \\263
+    ;
+    const allocator = std.testing.allocator;
+    const actual = try day01a(allocator, file);
+    defer allocator.free(actual);
+    std.debug.print("{s}\n", .{actual});
+    try std.testing.expectEqualStrings("7", actual);
 }
--- src/main.zig
+++ src/main.zig
@@ -2,45 +2,23 @@ const std = @import("std");
 const day01 = @import("day01.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
     const allocator = std.heap.page_allocator;
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
     for (args[1..]) |arg| {
-        if (std.mem.eql(u8, arg, "1")) {
-            try day01.main();
+        if (std.mem.eql(u8, arg, "1a")) {
+            const file = try getLinesFromFile(allocator, "src/day01.txt");
+            defer allocator.free(file);
+            const result = try day01.day01a(allocator, file);
+            defer allocator.free(result);
+            std.debug.print("Day 1a result: {s}", .{result});
         } else {
             std.debug.print("Got an unfinished day: {s}.\n", .{arg});
         }
     }
-
-    try bw.flush(); // don't forget to flush!
 }
 
-test "simple test" {
-    std.debug.print("Simple test.\n", .{});
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "a test" {
-    std.debug.print("another test.\n", .{});
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn getLinesFromFile(allocator: std.mem.Allocator, file_name: []const u8) ![]u8 {
+    return try std.fs.cwd().readFileAlloc(allocator, file_name, std.math.maxInt(usize));
 }
