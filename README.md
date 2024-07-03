a63646a5b2d84dcbb97c63d3f0abd2b68d778d8a
Eli Gukovsky <gukovskye@gmail.com>
2024-07-02 22:49:23 -0400

    Basic setup

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # AdventOfCode2021
 
 My first ever time trying Zig!
+
+To build:
+```
+zig build
+```
+
+To run (in this example, run day 1a and 1b):
+```
+zig build run -- 1a 1b
+```
+
+To test a specific day:
+```
+zig test src/day01.zig
+```
---
+++ src/day01.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Hello, {s}!\n", .{"world"});
+}
+
+test "simple day 1" {
+    std.debug.print("Simple test for day 1.\n", .{});
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
