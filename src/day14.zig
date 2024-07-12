0a5aa86a8cbd1c795a2d0520da747ca46f564010
Eli Gukovsky <gukovskye@gmail.com>
2024-07-12 15:41:35 -0400

    More focused on the problem difficulty, got some Zig HashMap exposure

--- src/day14.zig
+++ src/day14.zig
@@ -75,7 +75,81 @@ pub fn day14a(allocator: std.mem.Allocator, file: []const u8) ![]const u8 {
 }
 
 pub fn day14b(allocator: std.mem.Allocator, file: []const u8) ![]const u8 {
-    return std.fmt.allocPrint(allocator, "{s}", .{file[0..1]});
+    // Naive approach doesn't work anymore!
+    var file_iterator = std.mem.tokenizeScalar(u8, file, '\n');
+    const template = file_iterator.next().?;
+
+    var rules = std.StringHashMap(u8).init(allocator);
+    defer rules.deinit();
+
+    var pair_counts = std.StringHashMap(usize).init(allocator);
+    defer pair_counts.deinit();
+
+    while (file_iterator.next()) |line| {
+        try rules.put(line[0..2], line[6]);
+        try pair_counts.put(line[0..2], 0);
+    }
+
+    for (0..(template.len - 1)) |char_i| {
+        const pair = template[char_i .. char_i + 2];
+        pair_counts.getPtr(pair).?.* += 1;
+    }
+
+    for (0..40) |_| {
+        var next_pair_counts = std.StringHashMap(usize).init(allocator);
+        var pair_keys_iterator = pair_counts.keyIterator();
+        while (pair_keys_iterator.next()) |key| {
+            try next_pair_counts.put(key.*, 0);
+        }
+        defer {
+            pair_counts.deinit();
+            pair_counts = next_pair_counts.move();
+        }
+
+        var pair_counts_iterator = pair_counts.iterator();
+        while (pair_counts_iterator.next()) |entry| {
+            const middle_char = rules.get(entry.key_ptr.*).?;
+            const first_new_pair = ([2]u8{ entry.key_ptr.*[0], middle_char })[0..2];
+            const second_new_pair = ([2]u8{ middle_char, entry.key_ptr.*[1] })[0..2];
+            const number_of_the_existing_pair = entry.value_ptr.*;
+
+            next_pair_counts.getPtr(first_new_pair).?.* += number_of_the_existing_pair;
+            next_pair_counts.getPtr(second_new_pair).?.* += number_of_the_existing_pair;
+        }
+    }
+
+    var letter_counts = std.AutoHashMap(u8, usize).init(allocator);
+    defer letter_counts.deinit();
+    {
+        var pair_counts_iterator = pair_counts.iterator();
+        while (pair_counts_iterator.next()) |entry| {
+            const letter = entry.key_ptr.*[0];
+            // Use the first letter of the pair, just add the final letter at the end.
+            const letter_result = try letter_counts.getOrPut(letter);
+            if (letter_result.found_existing) {
+                letter_result.value_ptr.* += entry.value_ptr.*;
+            } else {
+                letter_result.value_ptr.* = entry.value_ptr.*;
+            }
+        }
+        letter_counts.getPtr(template[template.len - 1]).?.* += 1;
+    }
+
+    var min_count: usize = std.math.maxInt(usize);
+    var max_count: usize = std.math.minInt(usize);
+    {
+        var count_iterator = letter_counts.valueIterator();
+        while (count_iterator.next()) |count_ptr| {
+            if (count_ptr.* > max_count) {
+                max_count = count_ptr.*;
+            }
+            if (count_ptr.* < min_count) {
+                min_count = count_ptr.*;
+            }
+        }
+    }
+
+    return std.fmt.allocPrint(allocator, "{d}", .{max_count - min_count});
 }
 
 test "Day 14a" {
